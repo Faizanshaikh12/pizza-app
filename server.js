8f58198172d024973f54a5f674845a615d2d527f
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-10-10 11:18:21 +0530

    Added the larevel-mix webpack service

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 9000
 
+//Assets
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('home')
 })
