b0933cc22f413b27999558b38dedeab87b765bbf
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-10-10 15:13:56 +0530

    make cart page

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ const PORT = process.env.PORT || 9000
 //Assets
 app.use(express.static('public'));
 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
